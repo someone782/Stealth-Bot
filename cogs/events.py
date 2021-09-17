85845ba86cfc0f5f6e70d4c0983c79025fd19a66
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-17 23:42:52 +0200

    LAST EDIT I SWEAR

--- cogs/events.py
+++ cogs/events.py
@@ -114,6 +114,7 @@ class Events(commands.Cog):
     async def on_guild_join(self, guild):
         channel = self.client.get_channel(883658687867158529)
         embed = discord.Embed(title="I've been added to a guild", description=f"Guild name: {guild.name}\nGuild ID: {guild.id}\nGuild Owner: {guild.owner.mention} **|** {guild.owner} **|** {guild.owner.id}", timestamp=discord.utils.utcnow(), color=0x2F3136)
+        embed.set_thumbnail(url=guild.icon.url)
         embed.set_footer(text=f"I'm now in {len(self.client.guilds)} guilds", icon_url=self.client.user.avatar.url)
 
         await channel.send(embed=embed)
@@ -122,6 +123,7 @@ class Events(commands.Cog):
     async def on_guild_remove(self, guild):
         channel = self.client.get_channel(883658687867158529)
         embed = discord.Embed(title="I've been removed from a guild", description=f"Guild name: {guild.name}\nGuild ID: {guild.id}\nGuild Owner: {guild.owner.mention} **|** {guild.owner} **|** {guild.owner.id}", timestamp=discord.utils.utcnow(), color=0x2F3136)
+        embed.set_thumbnail(url=guild.icon.url)
         embed.set_footer(text=f"I'm now in {len(self.client.guilds)} guilds", icon_url=self.client.user.avatar.url)
 
         await channel.send(embed=embed)
