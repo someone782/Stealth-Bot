309df1e231d741ff587b95569cbbb97973b0cc06
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-18 14:36:33 +0200

    aaaa

--- cogs/help.py
+++ cogs/help.py
@@ -89,7 +89,7 @@ Written with `{count_python('.'):,}` lines.
             if command_signatures:
                 num = f"{iter}\U0000fe0f\U000020e3" if iter < 10 else "\U0001f51f"
                 cogindex.append(cog.qualified_name)
-                allcogs.append(f"{cog.description[0]} {prefix}help {cog.qualified_name}")
+                allcogs.append(f"> {prefix}help {cog.qualified_name}")
                 iter+=1
         nl = '\n'
         embed.add_field(name=f"<:category:882685952999428107> __**Available categories**__ **[{len(allcogs)}]**", value=f"""
