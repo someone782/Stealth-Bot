51deab18787070ca7b4d33ec2525475393121553
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-18 23:36:56 +0200

    hehe

---
+++ errors.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+
+class CommandDoesntExist(commands.CheckFailure):
+    pass
+
+class AuthorBlacklisted(commands.CheckFailure):
+    pass
+
+class TooLongPrefix(commands.CheckFailure):
+    pass
+
+class TooManyPrefixes(commands.CheckFailure):
+    pass
+
+class PrefixAlreadyExists(commands.CheckFailure):
+    pass
+
+class PrefixDoesntExist(commands.CheckFailure):
+    pass
+
+class KillYourself(commands.CheckFailure):
+    pass
