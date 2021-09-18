d4046aa5fee77b9d0fd187681571888f08859eb4
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-18 13:28:52 +0200

    renamed nsfw maid to nmaid and added maid cmd to images.py

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -80,9 +80,9 @@ class NSFW(commands.Cog):
 
         await ctx.reply(embed=embed)
         
-    @commands.command(aliases=['nsfw_maid', 'maid_nsfw'])
+    @commands.command(aliases=['maidn', 'nsfw_maid', 'maid_nsfw'])
     @commands.is_nsfw()
-    async def maid(self, ctx):
+    async def nmaid(self, ctx):
         async with aiohttp.ClientSession() as session:
             request = await session.get('https://api.waifu.im/nsfw/maid/')
             json = await request.json()
