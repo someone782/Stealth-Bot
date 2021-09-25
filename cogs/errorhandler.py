4135a863a7945fc92f2ca0091461d7a4b4be83fd
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-25 18:13:46 +0200

    bruh

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -85,7 +85,7 @@ class ErrorHandler(commands.Cog):
 
             if matches:
                 embed = discord.Embed(title="Yes")
-                confirm = await ctx.confirm(message=f"I couldn't find a command called `{ctx.invoked_with}`.\nDid you mean `{f'{matches[0]}`?' if matches else ''}`?", embed=embed
+                confirm = await ctx.confirm(message=f"I couldn't find a command called `{ctx.invoked_with}`.\nDid you mean `{f'{matches[0]}`?' if matches else ''}`?", embed=embed,
                                             delete_after_confirm=True, delete_after_timeout=True, delete_after_cancel=True,
                                             buttons=(('<:greenTick:596576670815879169>', f'', discord.ButtonStyle.gray), ('🗑', None, discord.ButtonStyle.red)), timeout=15)
 
