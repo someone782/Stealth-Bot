f39f2a7410f2cca6dfcde1f7c2925554ccc26e1d
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-28 21:44:36 +0200

    bruhg

--- cogs/misc.py
+++ cogs/misc.py
@@ -185,7 +185,7 @@ class Misc(commands.Cog):
                 # await member.add_roles(discord.utils.get(member.guild.roles, name=role))
 
     @commands.command(help="Shows the server address and port of ClassicSMP", aliases=['address', 'classicsmp'])
-    @helpers.is_csmp_server()N
+    @helpers.is_csmp_server()
     async def ip(self, ctx):
         embed = discord.Embed(title="ClassicSMP IP", description="""
 ```diff
