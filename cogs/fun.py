8c8603a54083849f63801309105d5a4ffea8a9f0
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-09-30 19:44:30 +0200

    yeet

--- cogs/fun.py
+++ cogs/fun.py
@@ -881,6 +881,10 @@ Original text: {text}
 
             message = await ctx.send("Starting RPG...")
             
+            confirm = await ctx.confirm(message=f"I couldn't find a command called `{ctx.invoked_with}`.\nDid you mean `{f'{matches[0]}' if matches else ''}`?",
+                                        delete_after_confirm=True, delete_after_timeout=True, delete_after_cancel=True,
+                                        buttons=(('<:greenTick:596576670815879169>', f'', discord.ButtonStyle.gray), ('🗑', None, discord.ButtonStyle.red)), timeout=15)
+            
             await message.edit("Do you want to turn hard-code more on? `yes/no`")
             
             try:
