4ec8a12082e6fe53a1a4ed4de41a01a7c239f404
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-10-02 20:13:18 +0200

    yeet

--- cogs/mod.py
+++ cogs/mod.py
@@ -113,7 +113,7 @@ class Mod(commands.Cog):
         
         for ban in guildBans:
             
-            bans.append(f"{ban.user} ({ban.user.id}) **|** {ban.reason}")
+            bans.append(f"{ban.user}")
             
         paginator = ViewMenuPages(source=ServerBansEmbedPage(bans,guild), clear_reactions_after=True)
         page = await paginator._source.get_page(0)
