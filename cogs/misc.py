24f60bacea8a64ec269281c32856f184b82b163f
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-10-02 15:24:43 +0200

    yeet

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,6 +13,9 @@ from typing import Optional
 import typing
 from discord.ext import commands
 
+from discord.ext import menus
+from discord.ext.menus.views import ViewMenuPages
+
 def setup(client):
     client.add_cog(Misc(client))
     
