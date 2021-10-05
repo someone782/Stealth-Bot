f987014f904d2a4fa3b853e0ff6a4d1e24c0a89d
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-10-05 15:59:12 +0200

    yeet

--- __main__.py
+++ __main__.py
@@ -277,7 +277,8 @@ client = StealthBot(
     case_insensitive=True,
     help_command=None,
     enable_debug_events=True,
-    strip_after_prefix=True) # Initializes the client object
+    strip_after_prefix=True,
+    shard=3) # Initializes the client object
 
 client.tracker = DiscordUtils.InviteTracker(client) # Initializes the tracker object
 client.owner_ids = [564890536947875868, 555818548291829792] # 349373972103561218 (LeoCx1000) # 555818548291829792 (Vicente0670)
