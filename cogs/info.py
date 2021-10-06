2601a60b42d630e1a5b44cf147f932c9a148ab46
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-10-06 15:52:40 +0200

    yeet

--- cogs/info.py
+++ cogs/info.py
@@ -25,6 +25,62 @@ import asyncio
 
 from discord.ext import menus
 from discord.ext.menus.views import ViewMenuPages
+import re
+import zlib
+
+def finder(text, collection, *, key=None, lazy=True):
+    suggestions = []
+    text = str(text)
+    pat = '.*?'.join(map(re.escape, text))
+    regex = re.compile(pat, flags=re.IGNORECASE)
+    for item in collection:
+        to_search = key(item) if key else item
+        r = regex.search(to_search)
+        if r:
+            suggestions.append((len(r.group()), r.start(), item))
+
+    def sort_key(tup):
+        if key:
+            return tup[0], tup[1], key(tup[2])
+        return tup
+
+    if lazy:
+        return (z for _, _, z in sorted(suggestions, key=sort_key))
+    else:
+        return [z for _, _, z in sorted(suggestions, key=sort_key)]
+
+
+class SphinxObjectFileReader:
+    # Inspired by Sphinx's InventoryFileReader
+    BUFSIZE = 16 * 1024
+
+    def __init__(self, buffer):
+        self.stream = io.BytesIO(buffer)
+
+    def readline(self):
+        return self.stream.readline().decode('utf-8')
+
+    def skipline(self):
+        self.stream.readline()
+
+    def read_compressed_chunks(self):
+        decompressor = zlib.decompressobj()
+        while True:
+            chunk = self.stream.read(self.BUFSIZE)
+            if len(chunk) == 0:
+                break
+            yield decompressor.decompress(chunk)
+        yield decompressor.flush()
+
+    def read_compressed_lines(self):
+        buf = b''
+        for chunk in self.read_compressed_chunks():
+            buf += chunk
+            pos = buf.find(b'\n')
+            while pos != -1:
+                yield buf[:pos].decode('utf-8')
+                buf = buf[pos + 1:]
+                pos = buf.find(b'\n')
 
 # bytes pretty-printing
 UNITS_MAPPING = [
@@ -137,6 +193,124 @@ class Info(commands.Cog):
     "<:info:888768239889424444> | All informative commands like serverinfo, userinfo and more!"
     def __init__(self, client):
         self.client = client
+        
+    async def build_rtfm_lookup_table(self, page_types):
+        cache = {}
+        for key, page in page_types.items():
+            sub = cache[key] = {}
+            async with self.client.session.get(page + '/objects.inv') as resp:
+                if resp.status != 200:
+                    raise RuntimeError('Cannot build rtfm lookup table, try again later.')
+
+                stream = SphinxObjectFileReader(await resp.read())
+                cache[key] = self.parse_object_inv(stream, page)
+
+        self._rtfm_cache = cache
+
+    def parse_object_inv(self, stream, url):
+        # key: URL
+        # n.b.: key doesn't have `discord` or `discord.ext.commands` namespaces
+        result = {}
+
+        # first line is version info
+        inv_version = stream.readline().rstrip()
+
+        if inv_version != '# Sphinx inventory version 2':
+            raise RuntimeError('Invalid objects.inv file version.')
+
+        # next line is "# Project: <name>"
+        # then after that is "# Version: <version>"
+        projname = stream.readline().rstrip()[11:]
+        version = stream.readline().rstrip()[11:]
+
+        # next line says if it's a zlib header
+        line = stream.readline()
+        if 'zlib' not in line:
+            raise RuntimeError('Invalid objects.inv file, not z-lib compatible.')
+
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        for line in stream.read_compressed_lines():
+            match = entry_regex.match(line.rstrip())
+            if not match:
+                continue
+
+            name, directive, prio, location, dispname = match.groups()
+            domain, _, subdirective = directive.partition(':')
+            if directive == 'py:module' and name in result:
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
+                # two inventory entries are created
+                # for Python modules, and the first
+                # one is correct
+                continue
+
+            # Most documentation pages have a label
+            if directive == 'std:doc':
+                subdirective = 'label'
+
+            if location.endswith('$'):
+                location = location[:-1] + name
+
+            key = name if dispname == '-' else dispname
+            prefix = f'{subdirective}:' if domain == 'std' else ''
+
+            if projname == 'discord.py':
+                key = key.replace('discord.ext.commands.', '').replace('discord.', '')
+
+            result[f'{prefix}{key}'] = os.path.join(url, location)
+
+        return result
+
+    async def do_rtfm(self, ctx, key, obj):
+        page_types = {
+            'latest': 'https://enhanced-dpy.readthedocs.io/en/latest/',
+            'latest-jp': 'https://discordpy.readthedocs.io/ja/latest',
+            'python': 'https://docs.python.org/3',
+            'python-jp': 'https://docs.python.org/ja/3',
+            'master': 'https://enhanced-dpy.readthedocs.io/en/latest/',
+        }
+
+        if obj is None:
+            await ctx.send(page_types[key])
+            return
+
+        if not hasattr(self, '_rtfm_cache'):
+            await ctx.trigger_typing()
+            await self.build_rtfm_lookup_table(page_types)
+
+        obj = re.sub(r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1', obj)
+
+        if key.startswith('latest'):
+            # point the abc.Messageable types properly:
+            q = obj.lower()
+            for name in dir(discord.abc.Messageable):
+                if name[0] == '_':
+                    continue
+                if q == name:
+                    obj = f'abc.Messageable.{name}'
+                    break
+
+        cache = list(self._rtfm_cache[key].items())
+
+        matches = finder(obj, cache, key=lambda t: t[0], lazy=False)[:8]
+
+        e = discord.Embed()
+        if len(matches) == 0:
+            return await ctx.send('Could not find anything. Sorry.')
+
+        e.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches)
+        await ctx.send(embed=e)
+
+    def transform_rtfm_language_key(self, ctx, prefix):
+        if ctx.guild is not None:
+            #                             日本語 category
+            if ctx.channel.category_id == 490287576670928914:
+                return prefix + '-jp'
+            #                    d.py unofficial JP   Discord Bot Portal JP
+            elif ctx.guild.id in (463986890190749698, 494911447420108820):
+                return prefix + '-jp'
+        return prefix
 
     @commands.command(
         help="Search lyrics of any song",
@@ -1272,3 +1446,22 @@ Tested verify command: Eiiknostv#2016
         view.add_item(item=item)
 
         await ctx.send(embed=embed, view=view)
+
+    @commands.group(help="Gives you a documentation link for a enhanced-discord.py entity in English", aliases=['rtfd'], invoke_without_command=True)
+    async def rtfm(self, ctx, *, entity : str=None):
+        key = self.transform_rtfm_language_key(ctx, 'latest')
+        await self.do_rtfm(ctx, key, entity)
+
+    @rtfm.command(help="Gives you a documentation link for a enhanced-discord.py entity in Japanese", name="jp", aliases=['ja', 'latest-jp', 'latest-ja', 'latest_jp', 'latest_ja'])
+    async def rtfm_jp(self, ctx, *, entity : str=None):
+        await self.do_rtfm(ctx, 'latest-jp', entity)
+
+    @rtfm.command(help="Gives you a documentation link for a Python entity in English", name="python", aliases=['py'])
+    async def rtfm_python(self, ctx, *, entity : str=None):
+        """Gives you a documentation link for a Python entity."""
+        key = self.transform_rtfm_language_key(ctx, 'python')
+        await self.do_rtfm(ctx, key, entity)
+
+    @rtfm.command(help="Gives you a documentation link for a Python entity in Japanese", name="py-jp", aliases=['py-ja', 'py_ja', 'ja-py', 'ja_py'])
+    async def rtfm_python_jp(self, ctx, *, entity : str=None):
+        await self.do_rtfm(ctx, 'python-jp', entity)
