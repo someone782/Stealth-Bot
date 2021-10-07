595546835ce04227028b50a9c1e6cb8a1ac752fd
Ender2K89 <68495763+Ender2K89@users.noreply.github.com>
2021-10-07 19:57:44 +0200

    yeet

--- cogs/info.py
+++ cogs/info.py
@@ -204,12 +204,12 @@ class Stuff(discord.ui.View):
         self.add_item(discord.ui.Button(emoji="<:invite:895688440639799347>", label='Invite me', url=url))
         self.add_item(discord.ui.Button(emoji="<:github:895688440492986389>", label='Source code', url="https://github.com/Ender2K89/Stealth-Bot"))
 
-    @discord.ui.button(label="Vote", emoji="<:dbl:757235965629825084>", timeout=15, style=discord.ButtonStyle.gray)
+    @discord.ui.button(label="Vote", emoji="<:dbl:757235965629825084>", style=discord.ButtonStyle.gray)
     async def vote(self, button : discord.ui.Button, interaction : discord.Interaction):
         embed=discord.Embed(title="Vote for me")
         await interaction.response.send_message(embed=embed, ephemeral=True, view=VoteButtons())
         
-    @discord.ui.button(label="Delete", emoji="🗑️", timeout=15, style=discord.ButtonStyle.red)
+    @discord.ui.button(label="Delete", emoji="🗑️", style=discord.ButtonStyle.red)
     async def delete(self, button : discord.ui.Button, interaction : discord.Interaction):
         await interaction.message.delete()
         
